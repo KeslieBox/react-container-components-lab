4dd7d772734e58eec2486c2109cf486b302cec21
KeslieBox <kesliebox@gmail.com>
2021-05-18 16:10:15 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,22 @@
-// Code MovieReviews Here
+import React from 'react';
+
+// destructuring to get the key/value pairs from each review iterated through below
+const Review = ({ headline, byline, summary_short, link}) => {
+    return (
+        <div key={headline} className="review">
+            <header>
+                <a className="review-link" href={link.url}>{headline}</a><br/>
+            </header>
+            <span className="author">{byline}</span>
+            <blockquote>{summary_short}</blockquote>
+        </div>
+    )
+}
+
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
